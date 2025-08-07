43f2be6cb6e000e3cc333e634425a8ac8e7b8f62
David Pugmire <dpugmire@gmail.com>
2025-08-07 07:58:16 -0400

    Spline interp for uniform and rectilinear grids.

---
+++ viskores/cont/SplineEvaluateRectilinearGrid.cxx
@@ -0,0 +1,50 @@
+//============================================================================
+//  The contents of this file are covered by the Viskores license. See
+//  LICENSE.txt for details.
+//
+//  By contributing to this file, all contributors agree to the Developer
+//  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
+//============================================================================
+
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
+
+#include <viskores/cont/CellSetStructured.h>
+#include <viskores/cont/SplineEvaluateRectilinearGrid.h>
+
+namespace viskores
+{
+namespace cont
+{
+
+viskores::exec::SplineEvaluateRectilinearGrid SplineEvaluateRectilinearGrid::PrepareForExecution(
+  viskores::cont::DeviceAdapterId device,
+  viskores::cont::Token& token) const
+{
+  using RectCoordsType = viskores::cont::ArrayHandleCartesianProduct<
+    viskores::cont::ArrayHandle<viskores::FloatDefault>,
+    viskores::cont::ArrayHandle<viskores::FloatDefault>,
+    viskores::cont::ArrayHandle<viskores::FloatDefault>>;
+
+  if (!this->DataSet.GetCoordinateSystem(0).GetData().IsType<RectCoordsType>())
+    throw viskores::cont::ErrorBadType("Coordinates are not rectilinear type.");
+
+  viskores::cont::ArrayHandle<viskores::FloatDefault> fieldArray;
+  this->DataSet.GetField(this->FieldName).GetData().AsArrayHandle(fieldArray);
+
+  RectCoordsType coords;
+  coords = this->DataSet.GetCoordinateSystem(0).GetData().AsArrayHandle<RectCoordsType>();
+
+  return viskores::exec::SplineEvaluateRectilinearGrid(coords,
+                                                       fieldArray.PrepareForInput(device, token));
+}
+
+} //namespace cont
+} //namespace viskores
---
+++ viskores/cont/testing/UnitTestSplineEvaluate.cxx
@@ -0,0 +1,292 @@
+//============================================================================
+//  The contents of this file are covered by the Viskores license. See
+//  LICENSE.txt for details.
+//
+//  By contributing to this file, all contributors agree to the Developer
+//  Certificate of Origin Version 1.1 (DCO 1.1) as stated in DCO.txt.
+//============================================================================
+
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
+
+#include <random>
+#include <string>
+
+#include <viskores/Math.h>
+#include <viskores/cont/DataSet.h>
+#include <viskores/cont/Invoker.h>
+#include <viskores/cont/SplineEvaluateRectilinearGrid.h>
+#include <viskores/cont/SplineEvaluateUniformGrid.h>
+
+#include <viskores/cont/DataSetBuilderExplicit.h>
+#include <viskores/cont/DataSetBuilderRectilinear.h>
+#include <viskores/cont/DataSetBuilderUniform.h>
+
+#include <viskores/cont/testing/Testing.h>
+
+#include <viskores/worklet/WorkletMapField.h>
+#include <viskores/worklet/WorkletMapTopology.h>
+
+#include <viskores/filter/resampling/Probe.h>
+
+
+namespace
+{
+
+VISKORES_EXEC
+viskores::FloatDefault EvaluateNormalizedGyroid(const viskores::Vec3f& point)
+{
+  //f(x,y,z)=sin(2πx)cos(2πy)+sin(2πy)cos(2πz)+sin(2πz)cos(2πx)
+  return viskores::Sin(viskores::TwoPi() * point[0]) * viskores::Cos(viskores::TwoPi() * point[1]) +
+    viskores::Sin(viskores::TwoPi() * point[1]) * viskores::Cos(viskores::TwoPi() * point[2]) +
+    viskores::Sin(viskores::TwoPi() * point[2]) * viskores::Cos(viskores::TwoPi() * point[0]);
+}
+
+class EvalWorklet : public viskores::worklet::WorkletMapField
+{
+public:
+  EvalWorklet() {}
+
+  using ControlSignature = void(FieldIn pointsIn, ExecObject splineEval, FieldOut results);
+  using ExecutionSignature = void(_1, _2, _3);
+  using InputDomain = _1;
+
+  template <typename PointType, typename SplineEvalType, typename ResultType>
+  VISKORES_EXEC void operator()(const PointType& pointIn,
+                                const SplineEvalType& splineEval,
+                                ResultType& result) const
+  {
+    splineEval.Evaluate(pointIn, result);
+  }
+};
+
+class GenerateData : public viskores::worklet::WorkletMapField
+{
+public:
+  using ControlSignature = void(FieldIn pointsIn, FieldOut results);
+  //using ControlSignature = void(FieldIn coords, FieldOut fieldOut);
+  using ExecutionSignature = void(_1, _2);
+
+  VISKORES_EXEC
+  void operator()(const viskores::Vec3f& point, viskores::FloatDefault& value) const
+  {
+    // Example function to generate a field value based on the point coordinates.
+    // This is a placeholder for the actual field generation logic.
+    value = EvaluateNormalizedGyroid(point);
+  }
+};
+
+std::vector<viskores::cont::DataSet> MakeDataSet3D(const std::vector<viskores::Id3>& dims)
+{
+  viskores::cont::DataSetBuilderUniform builder;
+  viskores::Vec3f origin(0.0f, 0.0f, 0.0f);
+
+  std::vector<viskores::cont::DataSet> dataSets;
+  for (const auto& d : dims)
+  {
+    viskores::Vec3f spacing(1.0 / (d[0] - 1), 1.0 / (d[1] - 1), 1.0 / (d[2] - 1));
+    auto ds = builder.Create(d, origin, spacing);
+
+    viskores::cont::Invoker invoker;
+    viskores::cont::ArrayHandle<viskores::FloatDefault> fieldArray;
+    invoker(GenerateData{}, ds.GetCoordinateSystem(), fieldArray);
+    ds.AddPointField("field", fieldArray);
+    dataSets.push_back(ds);
+  }
+
+  return dataSets;
+}
+
+template <typename F>
+static void fillCoords(std::vector<viskores::FloatDefault>& coords, viskores::Id N, F g)
+{
+  coords.resize(N);
+  for (viskores::Id i = 0; i < N; ++i)
+  {
+    viskores::FloatDefault t = static_cast<viskores::FloatDefault>(i) / (N - 1);
+    coords[i] = g(t);
+  }
+  coords[N - 1] = 1.0f;
+}
+
+std::vector<viskores::cont::DataSet> MakeRectDataSet3D(const std::vector<viskores::Id3>& dims)
+{
+  std::vector<viskores::cont::DataSet> dataSets;
+
+  for (const auto& d : dims)
+  {
+    for (int i = 0; i < 4; i++)
+    {
+      viskores::cont::DataSetBuilderRectilinear builder;
+      std::vector<viskores::FloatDefault> xcoords, ycoords, zcoords;
+
+      if (i == 0)
+      {
+        //uniform spacing.
+        fillCoords(xcoords, d[0], [](viskores::FloatDefault t) { return t; });
+        fillCoords(ycoords, d[1], [](viskores::FloatDefault t) { return t; });
+        fillCoords(zcoords, d[2], [](viskores::FloatDefault t) { return t; });
+      }
+      else if (i == 1)
+      {
+        //quadratic clustering near 0.
+        fillCoords(xcoords, d[0], [](viskores::FloatDefault t) { return t * t; });
+        fillCoords(ycoords, d[1], [](viskores::FloatDefault t) { return t * t; });
+        fillCoords(zcoords, d[2], [](viskores::FloatDefault t) { return t * t; });
+      }
+      else if (i == 2)
+      {
+        //quadratic clustering near 1.
+        fillCoords(xcoords, d[0], [](viskores::FloatDefault t) { return 1 - (1 - t) * (1 - t); });
+        fillCoords(ycoords, d[1], [](viskores::FloatDefault t) { return 1 - (1 - t) * (1 - t); });
+        fillCoords(zcoords, d[2], [](viskores::FloatDefault t) { return 1 - (1 - t) * (1 - t); });
+      }
+      else if (i == 3)
+      {
+        //exponential
+        fillCoords(xcoords,
+                   d[0],
+                   [](viskores::FloatDefault t)
+                   {
+                     const viskores::FloatDefault k = 3;
+                     return (std::exp(k * t) - 1) / (std::exp(k) - 1);
+                   });
+        fillCoords(ycoords,
+                   d[1],
+                   [](viskores::FloatDefault t)
+                   {
+                     const viskores::FloatDefault k = 3;
+                     return (std::exp(k * t) - 1) / (std::exp(k) - 1);
+                   });
+        fillCoords(zcoords,
+                   d[2],
+                   [](viskores::FloatDefault t)
+                   {
+                     const viskores::FloatDefault k = 3;
+                     return (std::exp(k * t) - 1) / (std::exp(k) - 1);
+                   });
+      }
+
+      auto ds = builder.Create(xcoords, ycoords, zcoords);
+      viskores::cont::Invoker invoker;
+      viskores::cont::ArrayHandle<viskores::FloatDefault> fieldArray;
+      invoker(GenerateData{}, ds.GetCoordinateSystem(), fieldArray);
+      ds.AddPointField("field", fieldArray);
+      dataSets.push_back(ds);
+    }
+  }
+
+  return dataSets;
+}
+
+viskores::cont::ArrayHandle<viskores::FloatDefault> CreateLinearInterpolationResult(
+  const viskores::cont::DataSet& ds,
+  const std::vector<viskores::Vec3f>& points)
+{
+  viskores::cont::DataSetBuilderExplicit builder;
+  std::vector<viskores::Id> ids(points.size());
+  std::iota(ids.begin(), ids.end(), 0);
+
+  auto ptDataSet = builder.Create(points, viskores::CellShapeTagVertex(), 1, ids);
+
+  viskores::filter::resampling::Probe probe;
+  probe.SetActiveField("field");
+  probe.SetGeometry(ptDataSet);
+  auto output = probe.Execute(ds);
+
+  viskores::cont::ArrayHandle<viskores::FloatDefault> result;
+  ds.GetField("field").GetData().AsArrayHandle(result);
+  return result;
+}
+
+template <typename SplineEvalType>
+void RunTest(SplineEvalType& splineEval,
+             const std::vector<viskores::Vec3f>& points,
+             const std::vector<viskores::FloatDefault>& expectedValues,
+             const viskores::cont::DataSet& ds)
+{
+  viskores::cont::Invoker invoke;
+  viskores::cont::ArrayHandle<viskores::Vec3f> pointsHandle =
+    viskores::cont::make_ArrayHandle(points, viskores::CopyFlag::On);
+  viskores::cont::ArrayHandle<viskores::FloatDefault> results;
+
+  //Compare against linear interpolation.
+  auto linearResult = CreateLinearInterpolationResult(ds, points);
+
+  EvalWorklet evalWorklet;
+  invoke(evalWorklet, pointsHandle, splineEval, results);
+
+  auto splinePortal = results.ReadPortal();
+  auto linearPortal = linearResult.ReadPortal();
+  for (viskores::Id i = 0; i < splinePortal.GetNumberOfValues(); i++)
+  {
+    auto truth = expectedValues[static_cast<std::size_t>(i)];
+    auto linearValue = linearPortal.Get(i);
+    auto splineValue = splinePortal.Get(i);
+
+    auto diffLinear = viskores::Abs(truth - linearValue);
+    auto diffSpline = viskores::Abs(truth - splineValue);
+    auto factor = diffLinear / diffSpline;
+
+    // spline interpolation should be better.
+    VISKORES_TEST_ASSERT(factor > 10.0f, "Interpolation value difference outside of error bounds.");
+  }
+}
+
+std::vector<viskores::Vec3f> CreateRandomVec3f(std::size_t N)
+{
+  std::mt19937 gen(12);
+  std::uniform_real_distribution<float> dist(0.05f, 0.95f);
+
+  std::vector<viskores::Vec3f> v;
+  v.reserve(N);
+
+  for (std::size_t i = 0; i < N; ++i)
+    v.emplace_back(viskores::Vec3f(dist(gen), dist(gen), dist(gen)));
+
+  return v;
+}
+
+void DoSplineEvalTest()
+{
+  viskores::cont::ArrayHandle<viskores::Vec3f> points;
+  viskores::cont::ArrayHandle<viskores::FloatDefault> results;
+  EvalWorklet evalWorklet;
+  auto pointData = CreateRandomVec3f(5);
+
+  std::vector<viskores::FloatDefault> expectedValues;
+  for (const auto& pt : pointData)
+    expectedValues.push_back(EvaluateNormalizedGyroid(pt));
+
+  std::vector<viskores::Id3> dims = { { 20, 20, 20 }, { 50, 50, 50 }, { 20, 40, 60 } };
+
+  auto dsUniform = MakeDataSet3D(dims);
+  auto dsRect = MakeRectDataSet3D(dims);
+
+  std::cout << "Uniform datasets." << std::endl;
+  for (const auto& ds : dsUniform)
+  {
+    viskores::cont::SplineEvaluateUniformGrid evalUniform(ds, "field");
+    RunTest(evalUniform, pointData, expectedValues, ds);
+  }
+
+  std::cout << "Rectilinear datasets." << std::endl;
+  for (const auto& ds : dsRect)
+  {
+    viskores::cont::SplineEvaluateRectilinearGrid evalRect(ds, "field");
+    RunTest(evalRect, pointData, expectedValues, ds);
+  }
+}
+} // anonymous namespace
+
+int UnitTestSplineEvaluate(int argc, char* argv[])
+{
+  return viskores::cont::testing::Testing::Run(DoSplineEvalTest, argc, argv);
+}
